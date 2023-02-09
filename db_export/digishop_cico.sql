b12ab36f21121ee684eeb276c7612702d7362ef8
Joakim Lönnqvist <joakim.is.lonnqvist@gmail.com>
2023-02-09 22:25:42 +0200

    Update digishop_cico.sql

--- db_export/digishop_cico.sql
+++ db_export/digishop_cico.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 31, 2023 at 04:42 PM
+-- Generation Time: Feb 09, 2023 at 09:24 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `mileage_allowance` (
   `id` bigint(255) NOT NULL,
-  `user_id` bigint(20) NOT NULL,
+  `uuid` varchar(36) NOT NULL,
   `name` varchar(256) NOT NULL,
   `surname` varchar(256) NOT NULL,
   `starting_location` varchar(256) NOT NULL,
@@ -43,12 +43,9 @@ CREATE TABLE `mileage_allowance` (
 -- Dumping data for table `mileage_allowance`
 --
 
-INSERT INTO `mileage_allowance` (`id`, `user_id`, `name`, `surname`, `starting_location`, `ending_location`, `km_traveled`, `date_traveled`, `date_submitted`) VALUES
-(1, 80443495973464965, 'Joakim', 'Lönnqvist', 'Arabia', 'Herttoniemi', 11, '2023-01-27', '2023-01-27 13:24:52'),
-(2, 4634443, 'Jekku', 'Ahonen', 'Arabia', 'Digishop Herttoniemi', 11, '2023-01-18', '2023-01-27 21:45:19'),
-(3, 4634443, 'Jekku', 'Ahonen', 'Arabianranta', 'Vuosaari', 45, '2023-02-02', '2023-01-30 13:53:59'),
-(4, 8910534309924099, 'Joakim', 'Lönnqvist', 'Vuosaari', 'Arabia', 15, '2023-01-29', '2023-01-30 14:00:19'),
-(5, 80443495973464965, 'Joakim', 'Lönnqvist', 'Vuosaari', 'Arabia', 15, '2023-01-12', '2023-01-30 16:13:24');
+INSERT INTO `mileage_allowance` (`id`, `uuid`, `name`, `surname`, `starting_location`, `ending_location`, `km_traveled`, `date_traveled`, `date_submitted`) VALUES
+(1, '5c02788e-5926-4e54-ba1a-d7bf36f7e6cf', 'Joakim', 'Lönnqvist', 'sadsad', 'adsasd', 11, '2023-02-14', '2023-02-02 14:37:04'),
+(2, '5c02788e-5926-4e54-ba1a-d7bf36f7e6cf', 'Joakim', 'Lönnqvist', 'Digishop Arabia, Hämeentie, Helsinki, Finland', 'Digishop Herttoniemi, Linnanrakentajantie, Helsinki, Finland', 15, '2023-02-01', '2023-02-02 15:42:20');
 
 -- --------------------------------------------------------
 
@@ -58,7 +55,7 @@ INSERT INTO `mileage_allowance` (`id`, `user_id`, `name`, `surname`, `starting_l
 
 CREATE TABLE `time_tracking` (
   `id` bigint(255) NOT NULL,
-  `user_id` bigint(255) NOT NULL,
+  `uuid` varchar(36) NOT NULL,
   `name` varchar(255) NOT NULL,
   `surname` varchar(255) NOT NULL,
   `entryDateTime` varchar(255) NOT NULL,
@@ -66,29 +63,20 @@ CREATE TABLE `time_tracking` (
   `breakStart` varchar(255) NOT NULL DEFAULT '0000-00-00 00:00:00',
   `breakEnd` varchar(255) NOT NULL DEFAULT '0000-00-00 00:00:00',
   `breakTime` varchar(255) NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `comments` varchar(300) NOT NULL,
-  `files` varchar(255) NOT NULL
+  `comments` varchar(300) DEFAULT NULL,
+  `files` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `time_tracking`
 --
 
-INSERT INTO `time_tracking` (`id`, `user_id`, `name`, `surname`, `entryDateTime`, `exitDateTime`, `breakStart`, `breakEnd`, `breakTime`, `comments`, `files`) VALUES
-(1, 80443495973464965, 'Joakim', 'Lönnqvist', '27/01/2023, 13:23:27', '27/01/2023, 13:25:50', '13:24:04', '13:25:26', '00:01:22', '', ''),
-(2, 80443495973464965, 'Joakim', 'Lönnqvist', '27/01/2023, 13:47:40', '27/01/2023, 18:07:48', '14:38:11', '15:00:08', '00:21:57', '', ''),
-(3, 4634443, 'Jekku', 'Ahonen', '27/01/2023, 21:40:24', '27/01/2023, 21:42:55', '21:42:07', '21:42:13', '00:00:06', '', ''),
-(4, 4634443, 'Jekku', 'Ahonen', '30/01/2023, 13:02:18', '30/01/2023, 13:53:24', '13:53:05', '13:53:18', '00:00:13', '', ''),
-(5, 8910534309924099, 'Joakim', 'Lönnqvist', '30/01/2023, 13:59:58', '30/01/2023, 14:00:46', '14:00:35', '14:00:41', '00:00:06', '', ''),
-(6, 80443495973464965, 'Joakim', 'Lönnqvist', '30/01/2023, 15:03:59', '30/01/2023, 15:04:03', '', '', '', '', ''),
-(7, 80443495973464965, 'Joakim', 'Lönnqvist', '30/01/2023, 15:05:11', '30/01/2023, 15:05:21', '', '', '', '', ''),
-(8, 80443495973464965, 'Joakim', 'Lönnqvist', '30/01/2023, 15:35:59', '30/01/2023, 15:36:40', '15:36:24', '15:36:33', '00:00:09', '', ''),
-(9, 80443495973464965, 'Joakim', 'Lönnqvist', '30/01/2023, 15:41:53', '30/01/2023, 16:04:38', '', '', '', '', ''),
-(10, 80443495973464965, 'Joakim', 'Lönnqvist', '30/01/2023, 16:04:54', '30/01/2023, 16:05:05', '16:04:57', '16:05:01', '00:00:04', '', ''),
-(11, 80443495973464965, 'Joakim', 'Lönnqvist', '30/01/2023, 16:13:01', '30/01/2023, 16:13:49', '16:13:28', '16:13:35', '00:00:07', '', ''),
-(12, 80443495973464965, 'Joakim', 'Lönnqvist', '30/01/2023, 17:05:35', '30/01/2023, 17:05:36', '', '', '', '', ''),
-(13, 80443495973464965, 'Joakim', 'Lönnqvist', '30/01/2023, 17:09:29', '30/01/2023, 17:09:30', '', '', '', '', ''),
-(14, 80443495973464965, 'Joakim', 'Lönnqvist', '30/01/2023, 18:32:11', '30/01/2023, 18:32:34', '18:32:21', '18:32:28', '00:00:07', '', '');
+INSERT INTO `time_tracking` (`id`, `uuid`, `name`, `surname`, `entryDateTime`, `exitDateTime`, `breakStart`, `breakEnd`, `breakTime`, `comments`, `files`) VALUES
+(24, '5c02788e-5926-4e54-ba1a-d7bf36f7e6cf', 'Joakim', 'Lönnqvist', '08/02/2023, 11:50:09', '08/02/2023, 11:50:10', '', '', '', 'No comments', NULL),
+(25, '5c02788e-5926-4e54-ba1a-d7bf36f7e6cf', 'Joakim', 'Lönnqvist', '08/02/2023, 12:53:44', '08/02/2023, 12:53:45', '', '', '', 'No comments', NULL),
+(26, '5c02788e-5926-4e54-ba1a-d7bf36f7e6cf', 'Joakim', 'Lönnqvist', '08/02/2023, 13:03:53', '08/02/2023, 13:04:44', '', '', '', 'No comments', NULL),
+(27, '5c02788e-5926-4e54-ba1a-d7bf36f7e6cf', 'Joakim', 'Lönnqvist', '08/02/2023, 13:07:17', '08/02/2023, 13:07:18', '', '', '', 'No comments', NULL),
+(28, '5c02788e-5926-4e54-ba1a-d7bf36f7e6cf', 'Joakim', 'Lönnqvist', '09/02/2023, 16:04:34', '09/02/2023, 16:04:37', '', '', '', 'No comments', NULL);
 
 -- --------------------------------------------------------
 
@@ -98,7 +86,7 @@ INSERT INTO `time_tracking` (`id`, `user_id`, `name`, `surname`, `entryDateTime`
 
 CREATE TABLE `users` (
   `id` bigint(20) NOT NULL,
-  `user_id` bigint(20) NOT NULL,
+  `uuid` varchar(36) NOT NULL,
   `username` varchar(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `surname` varchar(100) NOT NULL,
@@ -112,11 +100,8 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `user_id`, `username`, `name`, `surname`, `email`, `password`, `date`, `is_admin`) VALUES
-(1, 80443495973464965, 'lonnjoa', 'Joakim', 'Lönnqvist', 'Joakim.is.lonnqvist@gmail.com', '$2y$10$RvEMQoK.8b5.wjFz7CrgouLd12SSGbEGdkwATPqESr09kEDM8ECO.', '2023-01-27 13:22:51', 1),
-(2, 88287365735011, 'trypzo', 'Joakim', 'Lönnqvist', 'Joakim.is.lonnqvist@gmail.com', '$2y$10$GqD8cUP4giSAA5O9jJl4Q.ENm9EWGJ/./kUu0hd8KS6c46q8pxGoW', '2023-01-27 14:10:18', 0),
-(3, 4634443, 'jekku43', 'Jekku', 'Ahonen', 'jekku.ahonen@gmail.com', '$2y$10$QFQGb2JJ7kUh18HvDDIAe.Neaam7T2sRZ64TnFUAXaKJFSRWidDBu', '2023-01-27 21:36:00', 1),
-(4, 8910534309924099, 'joakim.lonnqvist', 'Joakim', 'Lönnqvist', 'juki.lonnqvist@gmail.com', '$2y$10$5V1VrbLKd9.lR1DDQkGUEuXCIrpoaQ5emHwkWvDnZgSDPXSivgCNK', '2023-01-30 13:59:41', 0);
+INSERT INTO `users` (`id`, `uuid`, `username`, `name`, `surname`, `email`, `password`, `date`, `is_admin`) VALUES
+(3, '5c02788e-5926-4e54-ba1a-d7bf36f7e6cf', 'lonnjoa', 'Joakim', 'Lönnqvist', 'Joakim.is.lonnqvist@gmail.com', '$2y$10$i..NqAOYG2ehrTpSLqoEzOL/oMFsmn1ss71nUpK1raksvkJxHQoIO', '2023-02-02 13:45:09', 1);
 
 -- --------------------------------------------------------
 
@@ -126,7 +111,7 @@ INSERT INTO `users` (`id`, `user_id`, `username`, `name`, `surname`, `email`, `p
 
 CREATE TABLE `user_data` (
   `id` bigint(20) NOT NULL,
-  `user_id` bigint(20) NOT NULL,
+  `uuid` varchar(36) NOT NULL,
   `isActive` varchar(20) NOT NULL,
   `onBreak` varchar(20) NOT NULL,
   `entryDateTime` varchar(30) NOT NULL,
@@ -140,11 +125,8 @@ CREATE TABLE `user_data` (
 -- Dumping data for table `user_data`
 --
 
-INSERT INTO `user_data` (`id`, `user_id`, `isActive`, `onBreak`, `entryDateTime`, `exitDateTime`, `breakStart`, `breakEnd`, `breakTime`) VALUES
-(1, 80443495973464965, 'true', 'false', '30/01/2023, 19:59:25', '', '12:09:42', '14:28:51', '02:19:09'),
-(7, 88287365735011, 'false', 'false', '', '', '', '', ''),
-(11, 4634443, 'false', 'false', '', '', '', '', ''),
-(27, 8910534309924099, 'false', 'false', '', '', '', '', '');
+INSERT INTO `user_data` (`id`, `uuid`, `isActive`, `onBreak`, `entryDateTime`, `exitDateTime`, `breakStart`, `breakEnd`, `breakTime`) VALUES
+(3, '5c02788e-5926-4e54-ba1a-d7bf36f7e6cf', 'false', 'false', '', '', '', '', '');
 
 --
 -- Indexes for dumped tables
@@ -159,14 +141,14 @@ ALTER TABLE `mileage_allowance`
   ADD KEY `surname` (`surname`),
   ADD KEY `date_submitted` (`date_submitted`),
   ADD KEY `date_travelled` (`date_traveled`),
-  ADD KEY `id` (`user_id`) USING BTREE;
+  ADD KEY `id` (`uuid`) USING BTREE;
 
 --
 -- Indexes for table `time_tracking`
 --
 ALTER TABLE `time_tracking`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`),
+  ADD KEY `user_id` (`uuid`),
   ADD KEY `name` (`name`),
   ADD KEY `surname` (`surname`);
 
@@ -175,7 +157,7 @@ ALTER TABLE `time_tracking`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`),
+  ADD KEY `user_id` (`uuid`),
   ADD KEY `name` (`name`),
   ADD KEY `surname` (`surname`),
   ADD KEY `surname_2` (`surname`),
@@ -188,7 +170,7 @@ ALTER TABLE `users`
 --
 ALTER TABLE `user_data`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `user_id` (`user_id`);
+  ADD UNIQUE KEY `user_id` (`uuid`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -198,25 +180,25 @@ ALTER TABLE `user_data`
 -- AUTO_INCREMENT for table `mileage_allowance`
 --
 ALTER TABLE `mileage_allowance`
-  MODIFY `id` bigint(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `time_tracking`
 --
 ALTER TABLE `time_tracking`
-  MODIFY `id` bigint(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` bigint(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user_data`
 --
 ALTER TABLE `user_data`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

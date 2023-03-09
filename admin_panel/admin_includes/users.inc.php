2e2af887550b31c8eed7e2e8eaad8fa05491b723
jqvistt <joakim.is.lonnqvist@gmail.com>
2023-03-09 08:15:43 +0200

    Normal commit
    Shits working now...

--- admin_panel/admin_includes/users.inc.php
+++ admin_panel/admin_includes/users.inc.php
@@ -10,81 +10,82 @@ $user_data = check_login($con);
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Users</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Users</title>
+    </head>
 
-<body>
-    <h1 id="main-header">Users</h1>
+    <body>
+        <h1 id="main-header">Users</h1>
 
-    <br>
+        <br>
 
-    <div class="table-box">
-        <table id="users-table">
-            <thead>
-                <tr>
-                    <td>UUID</td>
-                    <td>Username</td>
-                    <td>Full Name</td>
-                    <td>Date Registered</td>
-                    <td>Options</td>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
+        <div class="table-box">
+            <table id="users-table">
+                <thead>
+                    <tr>
+                        <td>UUID</td>
+                        <td>Username</td>
+                        <td>Full Name</td>
+                        <td>Date Registered</td>
+                        <td>Options</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
 
-                //SQL query to retrieve all the entries from mileage allowance table
-                $query1 = "SELECT * FROM users";
+                    //SQL query to retrieve all the entries from mileage allowance table
+                    $query1 = "SELECT * FROM users";
 
-                $result1 = mysqli_query($con, $query1);
+                    $result1 = mysqli_query($con, $query1);
 
-                while ($row = mysqli_fetch_assoc($result1)) {
+                    while ($row = mysqli_fetch_assoc($result1)) {
 
-                    echo "<tr>";
-                    echo "<td>" . $row['uuid'] . "</td>";
-                    echo "<td>" . $row['username'] . "</td>";
-                    echo "<td>" . $row['name'] . " " . $row['surname'] . "</td>";
-                    echo "<td>" . $row['date'] . "</td>";
-                    echo "<td title='Removes the specified user'>" . "<button data-id='" . $row['id'] . "'>Remove</button>" . "</td>";
-                    echo "<td title='Edits the specified row'>" . "<button>Edit</button>" . "</td>";
+                        echo "<tr>";
+                        echo "<td>" . $row['uuid'] . "</td>";
+                        echo "<td>" . $row['username'] . "</td>";
+                        echo "<td>" . $row['name'] . " " . $row['surname'] . "</td>";
+                        echo "<td>" . $row['date'] . "</td>";
+                        echo "<td title='Removes the specified user'>" . "<button data-id='" . $row['id'] . "'>Remove</button>" . "</td>";
+                        echo "<td title='Edits the specified row'>" . "<button>Edit</button>" . "</td>";
 
-                    echo "</tr>";
+                        echo "</tr>";
 
-                }
+                    }
 
-                ?>
-            </tbody>
-        </table>
-    </div>
+                    ?>
+                </tbody>
+            </table>
+        </div>
 
-    <!-- Other features or widgets can be added here -->
+        <!-- Other features or widgets can be added here -->
 
-    <script>
-        const removeButtons = document.querySelectorAll("#users-table button[data-id]");
+        <script>
 
-        removeButtons.forEach(button => {
-            button.addEventListener("click", () => {
-                const id = button.dataset.id;
+            removeButtons = document.querySelectorAll("#users-table button[data-id]");
 
-                // Prompt the user to confirm the deletion
-                if (confirm("Are you sure you want to delete this user? Changes cannot be restored!")) {
-                    // Send an AJAX request to the server to delete the corresponding row
-                    fetch(`admin_includes/delete_row.php?table_name=users&id=${id}`)
-                        .then(response => {
-                            if (response.ok) {
-                                // Remove the row from the table
-                                const tr = button.closest("tr");
-                                tr.parentNode.removeChild(tr);
-                            }
-                        });
-                }
+            removeButtons.forEach(button => {
+                button.addEventListener("click", () => {
+                    const id = button.dataset.id;
+
+                    // Prompt the user to confirm the deletion
+                    if (confirm("Are you sure you want to delete this user? Changes cannot be restored!")) {
+                        // Send an AJAX request to the server to delete the corresponding row
+                        fetch(`admin_includes/delete_row.php?table_name=users&id=${id}`)
+                            .then(response => {
+                                if (response.ok) {
+                                    // Remove the row from the table
+                                    const tr = button.closest("tr");
+                                    tr.parentNode.removeChild(tr);
+                                }
+                            });
+                    }
+                });
             });
-        });
-    </script>
+        </script>
 
-</body>
+    </body>
 
 </html>
--- js/JS_admin_panel.js
+++ js/JS_admin_panel.js
@@ -1,94 +1,95 @@
-window.addEventListener('load', function() {
-    const menuIcon = document.querySelector("#menu-icon");
-    const menuArrow = document.querySelector('.menu-arrow')
-    const sidebar = document.querySelector(".sidebar");
-    let currentContent = "";
-    let intervalId; // declare a variable to hold the interval ID
-    
-    function toggleSideBar() {
-        sidebar.classList.toggle("open");
-        menuArrow.classList.toggle("open");
-    };
+window.addEventListener("load", function () {
+  const removeButtons = "";
+  const menuIcon = document.querySelector("#menu-icon");
+  const menuArrow = document.querySelector(".menu-arrow");
+  const sidebar = document.querySelector(".sidebar");
+  let currentContent = "";
+  let intervalId; // declare a variable to hold the interval ID
 
-    function checkForChanges() {
-        $(".content").load("admin_includes/dashboard.inc.php", function() {
-            let newContent = $(".content").html(); // get the new content
-            if (newContent !== currentContent) { // check for changes
-                currentContent = newContent; // update the current content
-                clearInterval(intervalId); // clear the previous interval, if any
-                intervalId = setInterval(checkForChanges, 1000); // start the interval again
-            }
-        });
-    }
-    
+  function toggleSideBar() {
+    sidebar.classList.toggle("open");
+    menuArrow.classList.toggle("open");
+  }
 
+  function checkForChanges() {
+    $(".content").load("admin_includes/dashboard.inc.php", function () {
+      let newContent = $(".content").html(); // get the new content
+      if (newContent !== currentContent) {
+        // check for changes
+        currentContent = newContent; // update the current content
+        clearInterval(intervalId); // clear the previous interval, if any
+        intervalId = setInterval(checkForChanges, 1000); // start the interval again
+      }
+    });
+  }
+
+  toggleSideBar();
+
+  $(".content").load("admin_includes/dashboard.inc.php", function () {
+    currentContent = $(".content").html(); // get the current content
+    intervalId = setInterval(checkForChanges, 1000); // start the interval
+  });
+
+  menuIcon.addEventListener("click", function () {
     toggleSideBar();
+  });
 
-    $(".content").load("admin_includes/dashboard.inc.php", function() {
-        currentContent = $(".content").html(); // get the current content
-        intervalId = setInterval(checkForChanges, 1000); // start the interval
+  /* Dashboard Start */
+  document.getElementById("dashboard").addEventListener("click", function () {
+    clearInterval(intervalId); // clear the previous interval, if any
+
+    $(".content").load("admin_includes/dashboard.inc.php", function () {
+      currentContent = $(".content").html(); // get the current content
+      intervalId = setInterval(checkForChanges, 1000); // start the interval
     });
 
-    menuIcon.addEventListener("click", function() {
-        toggleSideBar();
-    });
+    console.log("dashboard pressed");
+    toggleSideBar();
+  });
+  /* Dashboard end */
 
-    /* Dashboard Start */
-    document.getElementById('dashboard').addEventListener('click', function() {
-        clearInterval(intervalId); // clear the previous interval, if any
+  /* Users Start */
+  document.getElementById("users").addEventListener("click", function () {
+    clearInterval(intervalId); // clear the previous interval, if any
 
-        $(".content").load("admin_includes/dashboard.inc.php", function() {
-            currentContent = $(".content").html(); // get the current content
-            intervalId = setInterval(checkForChanges, 1000); // start the interval
-        });
+    $(".content").load("admin_includes/users.inc.php");
 
-        console.log('dashboard pressed');
-        toggleSideBar();
-    });
-    /* Dashboard end */
+    console.log("users pressed");
+    toggleSideBar();
+  });
 
-    /* Users Start */
-    document.getElementById('users').addEventListener('click', function() {
-        clearInterval(intervalId); // clear the previous interval, if any
+  /* Users end */
 
-        $(".content").load("admin_includes/users.inc.php");
+  /* Mileages lStart */
+  document.getElementById("mileages").addEventListener("click", function () {
+    clearInterval(intervalId); // clear the previous interval, if any
 
-        console.log('users pressed');
-        toggleSideBar();
-    });
+    $(".content").load("admin_includes/mileages.inc.php");
 
-    /* Users end */
+    console.log("mileages pressed");
+    toggleSideBar();
+  });
+  /* Mileages end */
 
-    /* Mileages lStart */
-    document.getElementById('mileages').addEventListener('click', function() {
-        clearInterval(intervalId); // clear the previous interval, if any
+  /* Reports start */
+  document.getElementById("reports").addEventListener("click", function () {
+    clearInterval(intervalId); // clear the previous interval, if any
 
-        $(".content").load("admin_includes/mileages.inc.php");
+    $(".content").load("admin_includes/reports.inc.php");
 
-        console.log('mileages pressed');
-        toggleSideBar();
-    });
-    /* Mileages end */
+    console.log("reports pressed");
+    toggleSideBar();
+  });
+  /* Reports end */
 
-    /* Reports start */
-    document.getElementById('reports').addEventListener('click', function() {
-        clearInterval(intervalId); // clear the previous interval, if any
+  /* Settings start */
+  document.getElementById("settings").addEventListener("click", function () {
+    clearInterval(intervalId); // clear the previous interval, if any
 
-        $(".content").load("admin_includes/reports.inc.php");
+    $(".content").load("admin_includes/settings.inc.php");
 
-        console.log('reports pressed');
-        toggleSideBar();
-    });
-    /* Reports end */
-
-    /* Settings start */
-    document.getElementById('settings').addEventListener('click', function() {
-        clearInterval(intervalId); // clear the previous interval, if any
-
-        $(".content").load("admin_includes/settings.inc.php");
-
-        console.log('settings pressed');
-        toggleSideBar();
-    });
-    /* Settings end */
+    console.log("settings pressed");
+    toggleSideBar();
+  });
+  /* Settings end */
 });
